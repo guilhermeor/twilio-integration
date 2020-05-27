b3950b56ed7f0e6dc5031511fc3fc5c2e96b5010
Guilherme <guilhermeor.rs@gmail.com>
2020-05-27 17:19:34 -0300

    first commit

---
+++ Twilio.Console/Program.cs
@@ -0,0 +1,22 @@
+﻿using Twilio.Rest.Api.V2010.Account;
+
+namespace Twilio.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string accountSid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+            const string authToken = "your_auth_token";
+
+            TwilioClient.Init(accountSid, authToken);
+
+            var message = MessageResource.Create(
+                body: "Twilio test",
+                from: new Types.PhoneNumber("fromNumber"),
+                to: new Types.PhoneNumber("toNumber")
+            );
+            System.Console.WriteLine(message.Sid);
+        }
+    }
+}
